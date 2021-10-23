b3ced7dbaa3483ad46b0f91583213a8eedd102b3
Adrian <Owinmowe@gmail.com>
2021-10-23 18:16:07 -0300

    Added IA - Player Input Scripts.

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIA : MonoBehaviour
+{
+    SpinningTop top;
+    Vector3 startingPosition;
+    [SerializeField] float offsetToCenter = 1f;
+    private void Awake()
+    {
+        top = GetComponent<SpinningTop>();
+    }
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+        StartCoroutine(RandomMovement());
+    }
+    
+    IEnumerator RandomMovement() 
+    {
+        while (true)
+        {
+            float t = 0;
+            while (Vector3.Distance(startingPosition, transform.position) > offsetToCenter)
+            {
+                Vector3 dir = startingPosition - transform.position;
+                t += Time.deltaTime;
+                top.Move(dir);
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpinningTop))]
+public class PlayerInput : MonoBehaviour
+{
+    SpinningTop top;
+    private void Awake()
+    {
+        top = GetComponent<SpinningTop>();
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    void Update()
+    {
+        Vector3 pushForce;
+#if UNITY_EDITOR
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        pushForce = new Vector3(horizontal, 0, vertical);
+#elif UNITY_ANDROID
+        pushForce = new Vector3(Input.acceleration.x, 0, Input.acceleration.y);
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+#endif
+        top.Move(pushForce);
+    }
+}
